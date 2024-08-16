9a26ecad32796fc16dee8b58653a8fecdf896e07
rakelee <107320875+rakelee@users.noreply.github.com>
2024-08-16 15:57:33 -0700

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_DEFAULT
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_QWERTY
+#define MIRYOKU_NAV_DEFAULT
+#define MIRYOKU_CLIPBOARD_MAC
+#define MIRYOKU_LAYERS_FLIP
+#define MIRYOKU_MAPPING_DEFAULT
